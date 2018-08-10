83c53620f3fefac8f6e6e30e3b88582671f7bb0f
Tyler Long <tyler4long@gmail.com>
2018-08-10 17:35:26 +0800

    Create menu

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <Menu mode="horizontal" theme="primary" active-name="1">
+      <MenuItem name="log-out" @click.native="logOut">
+        <Icon type="md-exit" /> Log Out
+      </MenuItem>
+    </Menu>
     <ul>
       <li v-for="note in getNotes()">{{ note.title }}</li>
     </ul>
-    <Button type="success" @click="logOut">Log Out</Button>
   </div>
 </template>
 
