587e48b0638a162df6a948b262bc3f096a0c7f2c
Tyler Long <tyler4long@gmail.com>
2018-08-10 22:17:40 +0800

    Current note

--- src/components/App.vue
+++ src/components/App.vue
@@ -7,13 +7,15 @@
     </Menu>
     <Split v-model="split">
       <div slot="left" class="split-pane left-split-pane">
-        <Card v-for="note in getNotes()" :bordered="false" :key="note.id" class="note-card">
+        <Card v-for="note in getNotes()" :key="note.id" class="note-card" :class="{ 'active-note-card': currentNote.id === note.id }" @click.native="currentNote = note">
           <p slot="title">{{ note.title }}</p>
           <p>{{ note.preview }}</p>
         </Card>
       </div>
       <div slot="right" class="split-pane">
-        Right Pane
+        <div v-if="currentNote">
+          <h1>{{ currentNote.title }}</h1>
+        </div>
       </div>
     </Split>
   </div>
@@ -27,11 +29,25 @@ import rc from '../api/ringcentral'
 export default {
   data () {
     return {
-      split: 0.4
+      split: 0.4,
+      selected: undefined
     }
   },
   computed: {
-    ...mapGetters(['getNotes'])
+    ...mapGetters(['getNotes']),
+    currentNote: {
+      get: function () {
+        if (this.selected) {
+          return this.selected
+        }
+        if (this.getNotes().length > 0) {
+          return this.getNotes()[0]
+        }
+      },
+      set: function (value) {
+        this.selected = value
+      }
+    }
   },
   methods: {
     logOut: function () {
